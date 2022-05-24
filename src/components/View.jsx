349e16cf98d0cfe72f0f70550347f9e5a4aa00fe
Kerajärvi Kasper <kasper.kerajarvi@turku.fi>
2022-05-24 09:01:40 +0300

    Added event removing

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,21 +1,32 @@
-import { nanoid } from 'nanoid';
-import { Row, Col, Card, ProgressBar } from 'react-bootstrap';
+import { Row, Col, Card, ProgressBar, Button } from 'react-bootstrap';
 
 import EventModifier from './EventModifier';
 
 export default function View(props) {
+  function eventRemove(id) {
+    props.setEvents((prevEvents) =>
+      prevEvents.filter((event) => event.id !== id)
+    );
+  }
+
   const eventList = props.events.map((event) => {
-    // event.id = nanoid();
     return (
       <Card style={{ width: 'auto' }} key={event.id}>
         <Card.Body>
           <Card.Title>
-            {event.name} | {event.startTime} - {event.startTime}
+            {event.name} | {event.startTime} - {event.endTime}
           </Card.Title>
           <Card.Text>{event.description}</Card.Text>
-          <ProgressBar animated now={45} />
-          <EventModifier event={event} setEvents={props.setEvents} />
-          {/* <Button variant="primary">Edit</Button> */}
+          <ProgressBar animated now={50} />
+          <Col className="mt-3 mb-3 d-flex justify-content-between gap-5">
+            <EventModifier event={event} setEvents={props.setEvents} />
+            <Button
+              className="btn btn-danger"
+              onClick={() => eventRemove(event.id)}
+            >
+              Remove
+            </Button>
+          </Col>
         </Card.Body>
       </Card>
     );
